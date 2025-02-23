ab3e33c5029228a37e3c57eedd0dce8e211cc094
anuj836 <141640253+anuj1908@users.noreply.github.com>
2025-02-23 21:20:41 +0530

    Edited the kotlin and xml file

--- shared_preference/app/src/main/java/com/example/shared_preference/MainActivity.kt
+++ shared_preference/app/src/main/java/com/example/shared_preference/MainActivity.kt
@@ -1,52 +1,42 @@
 package com.example.shared_preference
 
-import android.app.Activity
+
 import android.os.Bundle
-import android.provider.ContactsContract.CommonDataKinds.Email
-import android.widget.Button
-import android.widget.EditText
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.shared_preference.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var usernameEditText: EditText
-    private lateinit var emailEditText: EditText
-    private lateinit var SaveButton: Button
-    private lateinit var LoadButton: Button
 
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val sharedPref = getSharedPreferences("mypref", MODE_PRIVATE)
         val editor = sharedPref.edit()
+
         binding.apply {
-            btnsave.setOnClickListener{
-                val username = usernameEditText.text.toString()
-                val email = emailEditText.text.toString()
+            btnsave.setOnClickListener {
+                val userName = edtusername.text.toString()
+                val email = edtemail.text.toString()
 
                 editor.apply {
-                    putString("username",username)
-                    putString("email",email)
+                    putString("user_name", userName)
+                    putString("email", email)
                     apply()
                 }
             }
-            btnload.setOnClickListener{
-                val username = sharedPref.getString("username", null)
+
+            btnload.setOnClickListener {
+                val username = sharedPref.getString("user_name", null)
                 val email = sharedPref.getString("email", null)
-                txtusername.text = username?:"No username found"
-                txtemail.text=email?:"No email found"
+
+                txtusername.text = username ?: "No username found"
+                txtemail.text = email ?: "No email found"
             }
         }
-
-
     }
 }
